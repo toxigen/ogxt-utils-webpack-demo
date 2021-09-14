83782469a7b204f04a8dd74636aae18bb16b5089
Konstantin V. Borisov <smeagol74@gmail.com>
2021-09-14 23:49:36 +0700

    * moved dist to docs for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist'
+        static: './docs'
     },
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     }
 }
